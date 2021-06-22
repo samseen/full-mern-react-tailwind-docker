ebc67d3b3c01dadf8e922d7e7d7692661ab4ead7
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-22 15:10:01 +0100

    Provided styling for different screen sizes of the hero section.

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 
 import McLarenCarImg from '../../../assets/images/mclaren-orange-big.png';
 import BlobImg from '../../../assets/images/blob.svg';
+import { SCREENS } from "../../components/responsive";
 
 const TopSectionContainer = styled.div`
     min-height: 400px;
@@ -82,6 +83,30 @@ const BlobContainer = styled.div`
         height: auto;
         max-height: max-content;
     }
+
+    @media (min-width: ${SCREENS.sm}) {
+        width: 40em;
+        max-height: 10em;
+        right: -9em;
+        top: -16em;
+        transform: rotate(-25deg);
+    }
+
+    @media (min-width: ${SCREENS.lg}) {
+        width: 50em;
+        max-height: 30em;
+        right: -7em;
+        top: -15em;
+        transform: rotate(-30deg);
+    }
+
+    @media (min-width: ${SCREENS.xl}) {
+        width: 70em;
+        max-height: 30em;
+        right: -15em;
+        top: -25em;
+        transform: rotate(-20deg);
+    }
 `;
 
 const StandaloneCar = styled.div`
@@ -95,6 +120,24 @@ const StandaloneCar = styled.div`
         width: auto;
         height: 100%;
         max-width: fit-content;
+    }
+
+    @media (min-width: ${SCREENS.sm}) {
+        height: 16em;
+        right: -6em;
+        top: -6em;
+    }
+
+    @media (min-width: ${SCREENS.lg}) {
+        height: 21em;
+        right: -8em;
+        top: -5em;
+    }
+
+    @media (min-width: ${SCREENS.xl}) {
+        height: 30em;
+        right: -13em;
+        top: -9em;
     }
 `;
 
