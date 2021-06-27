00822d103bcc027a5711e8cff051bbc8b514d539
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-27 21:10:47 +0100

    Created the CarContainer component.

--- src/app/components/cars/index.tsx
+++ src/app/components/cars/index.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import { ICar } from "../../../typings/car";
+import tw from 'twin.macro';
 
 interface ICarProps extends ICar {
 
 }
+
+const CarContainer = styled.div`
+    min-width: 16.5em;
+    min-height: 22.2em; 
+    max-height: 22.2em;
+    box-shadow: 0 1.3px 17px -2px rgba(0, 0, 0, 0.4);
+
+    ${tw`
+        flex
+        flex-col
+        item-center
+        p-3
+        pb-4
+        bg-white
+        rounded-md
+        m-1
+        sm:m-3
+        md:m-6
+    `};
+`;
 
 export function Car(props: ICarProps) {
 
