9592cb1be915e282a3c4f7389291a39c43e3f6fe
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-14 07:21:05 +0100

    Fetched data from the GraphQL API.

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-    uri: process.env.REACT_APP_API_URL as any,
+    uri: `${process.env.REACT_APP_API_URL}/graphql` as any,
     cache: new InMemoryCache(),
 });
+// export const apolloClient = new ApolloClient({
+//     uri: "http://localhost:9000/graphql" as any,
+//     cache: new InMemoryCache(),
+// });
