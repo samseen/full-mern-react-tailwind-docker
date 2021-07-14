a912244c91c4e87fd7382c1e1c8c098ee7b092e2
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-14 06:45:20 +0100

    Created an apollo service to model the fetched data for the UI.

---
+++ src/app/services/carService/index.ts
@@ -0,0 +1,21 @@
+import { apolloClient } from "../../graphql";
+import { GetCars_cars } from "./-watch/GetCars";
+import { GET_ALL_CARS } from "./queries";
+
+class CarService {
+
+    public async getCars(): Promise<GetCars_cars[]> {
+        const response = await apolloClient.query({ query: GET_ALL_CARS }).catch((err) => {
+            throw err;
+        });
+
+        if(response && response.data) {
+            return response.data as GetCars_cars[];
+        }
+
+        return [];
+    }
+
+}
+
+export default new CarService();
